fe98c793d3c5512ee6c1a2f4a8ca0480cab31c0f
ChandanaManthoju <115357238+ChandanaManthoju@users.noreply.github.com>
2023-09-12 11:28:16 +0530

    Delete homework.py

--- homework.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter youyr value:"))
-b=int(input("enter youyr value:"))
-c=int(input("enter youyr value:"))
-if a<b and a<c:
-    print(" a has minimum value")
-elif b<a and b<c:
-    print(" b has minimum value")
-else:
-    print("c has minimum value")
